0468034157c52141b0fe6c854a1ad68a3e00b933
Habil Yazıcı <habilyazici00@gmail.com>
2025-08-18 13:30:10 +0300

    a

--- new_python/3-Advanced Python Modülleri/3_os.py
+++ new_python/3-Advanced Python Modülleri/3_os.py
@@ -6,21 +6,20 @@ print(os.name)
 print("\n" + 20 * '-' + "\n")
 
 print("Mevcut dizin:", os.getcwd()) # get current working directory
-# C:\Users\habil\OneDrive\Belgeler\GitHub\ şeklinde gider normalde dizinler 
 os.chdir('C:\\Users\\habil\\OneDrive\\Belgeler\\')
 os.chdir('..')
 print("Mevcut dizin:", os.getcwd())
 print("\n" + 20 * '-' + "\n")
 
 os.chdir('C:\\Users\\habil\\OneDrive\\Belgeler\\GitHub\\basic-python\\new_python\\2-Hata ve dosya yönetimi')
-with open("newfile.txt", "r+", encoding="utf-8") as f:
-    f.seek(0)
-    okundu = f.read()
-    print(okundu)
+with open("newfile.txt", "r", encoding="utf-8") as f:
+    for i, line in enumerate(f, 1):
+        if i == 4:
+            print(line.strip())
+            break
 print("\n" + 20 * '-' + "\n")
 
-# dosya oluşturma
-os.chdir('C:\\Users\\habil\\OneDrive\\Belgeler\\GitHub\\basic-python\\new_python\\3-Advanced Python Modülleri')
+os.chdir(r'C:\Users\habil\OneDrive\Belgeler\GitHub\basic-python\new_python\3-Advanced Python Modülleri')
 try:
     if not os.path.exists("newdirectory"):
         os.mkdir("newdirectory")
@@ -39,8 +38,7 @@ finally:
     os.removedirs("yenidizin/yenidizin2")
     print("Dizinler temizlendi.")
 print("\n" + 20 * '-' + "\n")
- 
-# listeleme
+
 print("güncel klasör:", os.listdir())
 print("\n" + 20 * '-' + "\n")
 
@@ -57,7 +55,6 @@ print("\n" + 20 * '-' + "\n")
 print(os.getcwd())
 os.chdir('..')
 os.chdir('2-Hata ve dosya yönetimi')
-print(os.getcwd())
 result = os.stat("1_dosya_islemleri.py")
 print(result)
 print("\n" + 20 * '-' + "\n")
@@ -73,35 +70,34 @@ print("Değiştirilme tarihi:", result4)
 print("\n" + 20 * '-' + "\n")
 
 # os.system("start chrome")
+# os.system(r"start c:\Users")
+# os.system(r"start C:\Users\habil\AppData\Roaming\Spotify\Spotify.exe")
 yol = os.path.join("C:\\Users\\habil", "OneDrive", "Belgeler", "GitHub", "basic-python", "new_python", "2-Hata ve dosya yönetimi", "file1.txt")
 print("Dosya yolu:", yol) # pythonda iki ters eğik çizgi kullanılır çünkü tek ters eğik çizgi özel bir karakterdir kaçış karakteridir.
-os.system(f"notepad.exe {yol}")
+# os.system(f"notepad.exe {yol}")
 print("\n" + 20 * '-' + "\n")
 
 os.chdir('..')
 os.chdir('3-Advanced Python Modülleri')
-print(os.getcwd())
 
-base_dir = r"C:/Users/habil/OneDrive/Belgeler/GitHub/basic-python/new_python/3-Advanced Python Modülleri"
-file_py = os.path.join(base_dir, "3_os.py")
-file_py1 = os.path.join(base_dir, "3_os1.py")
+dosyaYolu = os.path.abspath("3_os.py")
+print("Dosya yolu:", dosyaYolu)
+dosyanın_klasörü = os.path.dirname(os.path.abspath("3_os.py"))
+print("Dosyanın klasörü:", dosyanın_klasörü)
+print("3_os1.py var mı   ", os.path.exists("3_os1.py"))
+print("bahsedilen klasör var mı    ", os.path.exists(dosyanın_klasörü))
+print("saadece klasör var mı   ", os.path.isdir(dosyanın_klasörü))
+print("saadece dosya var mı    ", os.path.isfile("3_os.py")) 
+print("\n" + 20 * '-')
 
-print(os.path.abspath(file_py))
-print(os.path.dirname(file_py))
-print(os.path.dirname(os.path.abspath(file_py)))
-print(os.path.exists(file_py1))
-print(os.path.exists(base_dir))
-print(os.path.isdir(base_dir))
-print(os.path.isfile(file_py))
-
-print(os.path.join("C:\\", "deneme", "deneme1"))
-split_result = os.path.split("C:\\deneme")
+print(os.path.join("C:\\", "deneme", "deneme1", "deneme2.py"))
+split_result = os.path.split("C:\\deneme\\deneme1\\deneme2.py")
+# join ve split herhangi bir dosya ve klasörün yolunu birleştirmek için tasarlanmıştır.
 print(split_result)
 print("Klasör yolu:", split_result[0])
-print("Klasör adı:", split_result[1])
+print("Dosya adı:", split_result[1])
 
-splitext_result = os.path.splitext(file_py)
+splitext_result = os.path.splitext("3_os.py")
 print(splitext_result)
 print("Dosya adı:", splitext_result[0])
 print("Dosya uzantısı:", splitext_result[1])
-
--- new_python/3-Advanced Python Modülleri/4_re.py
+++ new_python/3-Advanced Python Modülleri/4_re.py
@@ -1,229 +1,152 @@
+# regular expression, metin içinde arama, eşleşme, değiştirme gibi işlemleri kolaylaştırır
+# kaçış karakterleri ayrı regular expression ayrıdır \n yeni - \b dolayısıyla regex ararken r kullanmak kaçış karakteri oluşmasını engeller.
 import re
 
-# result = dir(re)
-
-# re module
+print(dir(re))
+print("\n" + 20 * '-')
 
 str = "Python Kursu: Python Programlama Rehberiniz | 40 saat"
-
-# re.findall()
-
-# result = re.findall("Python",str)
-# result = len(result)
-
-# re.split()
-
-# result = re.split(" ", str)
-# result = re.split("R", str)
-
-# re.sub()
-
-# result = re.sub(" ","-",str)
-# result = re.sub("\s","-",str)
-
-# re.search()
-
-# result = re.search("Python",str)
-
-# result = result.span()
-# result = result.start()
-# result = result.end()
-# result = result.group()
-# result = result.string
-
-# regular expression 
-
-"""
-
-    [] - Köşeli parantezler arasına yazılan bütün karakterler
-         aranır.
-
-         [abc] => a      : 1 match
-                  ac     : 2 match 
-                  Python : No matches
-
-         [a-e]  => [abcde]
-         [1-5]  => [12345]
-         [0-39] => [01239]   
-
-         [^abc] => abc dışındaki karakterler.
-         [^0-9] => rakam olmayan karakterler.
-
-"""
-
-result = re.findall("[abc]",str)
-result = re.findall("[sat]",str)
-result = re.findall("[a-e]", str)
-result = re.findall("[a-z]", str)
-result = re.findall("[0-5]", str)
-result = re.findall("[^abc]", str)
-result = re.findall("[^0-9]", str)
-
-"""
-    . - Her hangi bir tek karakteri belirtir.
-
-        .. => a    : No match
-              ab   : 1 match
-              abc  : 1 match
-              abcd : 2 matches
-
-    
-"""
-
-result = re.findall("...", str)
-result = re.findall("Py..on", str)
-
-"""
-    ^ - Belirtilen string karakterlerle başlıyor mu ?
-
-    ^a => a:    1 match
-          abc:  1 match
-          bac:  No match
-
-"""
-
-result = re.findall("^P",str)
-
-
-"""
-    $ - Belirtilen karakterle bitiyor mu ?
-
-    a$ => a      : 1 match
-          lamba  : 1 match
-          Python : No match 
-
-"""
-
-result = re.findall("t$",str)
-result = re.findall("saat$",str)
-result = re.findall("saatt$",str)
-
-"""
-     * - Bir karakterin sıfır ya da daha fazla sayıda olmasını 
-         kontrol eder.
-
-         ma*n => mn     : 1 match
-                 man    : 1 match
-                 maaan  : 1 match
-                 main   : No match (a' nın arkasına n gelmiyor.) 
-"""
-result = re.findall("sa*t",str)
-
-"""
-     + - Bir karakterin bir ya da daha fazla sayıda olmasını 
-         kontrol eder.
-
-         ma+n => mn     : No match
-                 man    : 1 match
-                 maaan  : 1 match
-                 main   : No match (a' nın arkasına n gelmiyor.) 
-"""
-
-result = re.findall("sa+t",str)
-
-"""
-    ? - Bir karakterin sıfır ya da bir kez olmasını 
-        kontrol eder.
-
-        ma+n => mn     : No match
-                man    : 1 match
-                maaan  : 1 match
-                main   : No match (a' nın arkasına n gelmiyor.) 
-"""
-
-result = re.findall("sa?t",str)
-
-"""
-    {} - Karakter sayısını kontrol eder.
-
-        al{2}   => a karakterinin arkasına l karakteri 2 kez tekrarlamalı.
-        al{2,3} => a karakterinin arkasına l karakteri en az 2 en fazla 3 kez tekrarlamalı.
-        [0-9]{2,4} => en az 2 en çok 4 basamaklı sayılar.
-"""
-result = re.findall("a{2}", str)
-result = re.findall("[0-9]{2}", str)
-
-"""
-    | - alternatif seçeneklerden birinin gerçekleşmesi gerekir.
-
-        a|b => a ya da b
-
-            cde =>    no match
-            ade =>    1 match
-            acdbea => 3 match 
-"""
-
-"""
-    () - gruplamak için kullanılır.
-
-         (a|b|c)xz => a,b,c karakterlerinin arkasına xz gelmelidir.
-"""
-
-
-
-"""
-    \ - Özel karakterleri aramamızı sağlar.
-        \$a => $ karakterinin arkasına a karakterinin arar. Yani
-               $ regular exp. engine tarafından yorumlanmaz.
-
-    \A - Belirtilen karakter string in başında mı ?
-         \Athe => the string in başındamı
-
-        result = re.findall("\APython", str)
-        result = re.findall("saat\Z", str)
-
-    \Z - Belirtilen karakter string in sonunda mı ?
-         the\Z => the string in sonunda mı
-
-    \b - Belirtilen karakter kelimenin in başında ya da sonunda mı ?
-         \bthe => the kelimenin in başında mı?
-         the\b => the kelimenin in sonunda mı?
-
-    \B - Belirtilen karakter kelimenin in başında ya da sonunda değil mı ?
-         \Bthe => the kelimenin in başında değil mi?
-         the\B => the kelimenin in sonunda değil mi?
-    
-    \d - [0-9] ile aynı anlama gelir yani rakamları arar.
-         \d => 12abc34
-
-    \D - [^0-9] ile aynı anlama gelir yani rakam olmayanları arar.
-         \D => 1ab44_50
-
-    \s - Boşluk karakterlerini arar.  
-    \S - Boşluk karakterleri dışındakiler.
-    \w - Alfabetik karakterler, rakamlar ve alt çizgi karakteri.
-    \W - \w nin tam tersi
-    
-"""
-
-
+result = re.findall("Python",str)
 print(result)
+print(len(result))
+print("\n" + 20 * '-')
 
-# x = 10
-# if x > 5:
-#     raise Exception("x 5 den büyük değer alamaz.")
-# def check_password(psw):
-#     import re
-#     if len(psw) < 8:
-#         raise Exception("parola en az 7 karakter olmalıdır.")
-#     elif not re.search("[a-z]", psw):
-#         raise Exception("parola küçük harf içermelidir.")
-#     elif not re.search("[A-Z]", psw):
-#         raise Exception("parola büyük harf içermelidir.")
-#     elif not re.search("[0-9]", psw):
-#         raise Exception("parola rakam içermelidir.")
-#     elif not re.search("[_@$]", psw):
-#         raise Exception("parola alpha numeric karakter içermelidir.")
-#     elif re.search("\s",psw):
-#         raise Exception("parola boşluk içermemelidir.")
-#     else:
-#         print("geçerli parola")
-# password = "1234567aA_"
-# try:
-#     check_password(password)
-# except Exception as ex:
-#     print(ex)
-# else:
-#     print("geçerli parola: else")
-# finally:
-#     print("validation tamamlandı.")
+print(re.split(" ", str, 3))
+print(re.split("R", str))
+print("\n" + 20 * '-')
+
+print(re.sub(" ","-",str))
+print(re.sub(r"\s","-",str))
+print("\n" + 20 * '-')
+
+# Kelimenin stringde ilk bulunan kısmının verilerini gösterir
+result = re.search("Python", str)
+print("result tipi: ", type(result))
+print("result nesnesi: ", result)
+print("Eşleşmenin span'ı: ", result.span())
+print("Eşleşmenin başlangıç index'i: ", result.start())
+print("Eşleşmenin bitiş index'i: ", result.end())
+print("Eşleşen metin: ", result.group())
+print("Aranan string: ", result.string)
+
+for match in re.finditer(r'Python', str, re.IGNORECASE):
+    print(match.span(), match.group())
+print("\n" + 20 * '-')
+
+# [] - Köşeli parantezler arasına yazılan bütün karakterler aranır.
+
+print("[a48c] harflerinden biri: ", re.findall("[a48c]",str))
+print("[a-e] aralığındaki karakterler: ", re.findall("[a-e]", str))
+print("[0-5] aralığındaki rakamlar: ", re.findall("[0-5]", str))
+print("Rakam harici karakterler: ", re.findall("[^0-9]", str))
+print("Harf hariç karakterler: ", re.findall("[^a-zA-Z]", str))
+print("\n" + 20 * '-')
+
+# . = Her hangi bir tek karakteri belirtir.
+print("ortada h.", re.findall(".h.", str))
+print("s ile başlar t ile biter.", re.findall("s..t", str))
+print("3 karakterli.", re.findall("...", str))
+print("P ile başlar n ile biter.", re.findall("P..h..n", str))
+print("\n" + 20 * '-')
+
+# ^ ve $ = string belirtilen karakterle başlayıp bitiyor mu?
+print("P ile başlıyor: ", re.findall("^P",str))
+print("a ile başlıyor: ", re.findall("^a",str))
+print("t ile bitiyor: ", re.findall("t$",str))
+print("s ile başlayıp t ile biten 4 karakterli: ", re.findall("s..t$",str))
+print("saatt ile bitiyor: ", re.findall("saatt$",str))
+print("\n" + 20 * '-')
+
+# * = Bir karakterin sıfır ya da daha fazla sayıda olmasını kontrol eder.
+print("a sıfır veya fazla.", re.findall("sa*t",str))
+print("a sıfır veya fazla.", re.findall("saa*t",str))
+print("a sıfır veya fazla.", re.findall("saaa*t",str))
+print("a ve i sıfır veya fazla.", re.findall("saaa*i*t",str))
+print("\n" + 20 * '-')
+
+# + = Bir karakterin bir ya da daha fazla sayıda olmasını kontrol eder.
+print("a bir veya fazla.", re.findall("sa+t",str))
+print("a bir veya fazla.", re.findall("saa+t",str))
+print("a bir veya fazla.", re.findall("saaa+t",str))
+print("\n" + 20 * '-')
+
+# ? = Bir karakterin sıfır ya da bir kez olmasını kontrol eder.
+print("a sıfır veya bir.", re.findall("sa?t",str))
+print("a sıfır veya bir.", re.findall("saa?t",str))
+print("a sıfır veya bir.", re.findall("saaa?t",str))
+print("a ve y sıfır veya bir.", re.findall("saaa?y?t",str))
+print("\n" + 20 * '-')
+
+# {} = Karakter sayısını kontrol eder.
+print("a iki kez.", re.findall("a{2}", str))
+print("a iki kez.", re.findall("sa{2}", str))
+print("a iki veya üç kez.", re.findall("sa{2,3}", str))
+print("a iki kez, t ile biter.", re.findall("sa{2}t", str))
+print("iki rakam.", re.findall("[0-9]{2}", str))
+print("iki-dört rakam.", re.findall("[0-9]{2,4}", str))
+print("\n" + 20 * '-')
+
+# | - alternatif seçeneklerden birinin gerçekleşmesi gerekir.
+print(re.findall("a|b", str))
+print("\n" + 20 * '-')
+
+# () - gruplamak için kullanılır.
+print(re.findall(r"(P|K)ython", str))
+print(re.findall(r"(P|K)ython\sKursu", str))
+print(re.findall(r"(Python|Programlama)\s", str))
+print(re.findall(r"(Python|Kursu)", str))
+print("\n" + 20 * '-')
+
+# \ karakteri, özel karakterleri (örneğin $) normal karakter gibi aramamızı sağlar. Yani $ işaretini aramak için \$ kullanılır.
+print(re.findall(r"\$a", "$abc a$ab"))
+print("\n" + 20 * '-')
+
+# \A ve \Z= Belirtilen karakter string in başında veya sonunda mı
+print(re.findall(r"\APython", "Python Kursu"))
+print(re.findall(r"saat\Z", str))
+print("\n" + 20 * '-')
+
+# \b: Belirtilen karakter kelimenin başında veya sonunda mı?
+print(re.findall(r"\bKursu", "Python Kursu"))
+print(re.findall(r"Kursu\b", "Python Kursu"))
+# \B: Belirtilen karakter kelimenin başında veya sonunda değil mi?
+print(re.findall(r"\Btho", "Python abdon"))
+print("\n" + 20 * '-')
+
+# \d = [0-9] ve \D = [^0-9] ile aynı anlama gelir
+print(re.findall(r"\d", "abc123de3f"))
+print(re.findall(r"\D", "1ab44_50"))
+print("\n" + 20 * '-')
+
+# \s = boşluk karakterini ve \S = boşluk karakteri harici
+print(re.findall(r"\s", "Python Kursu"))
+print(re.findall(r"\S", "Python Kursu"))
+print("\n" + 20 * '-')
+
+# \w = Alfabetik karakterler, rakamlar ve alt çizgi karakteri ve \W = bunların tam tersi
+print(re.findall(r"\w", "Python_123!"))
+print(re.findall(r"\W", "Python_123!"))
+print("\n" + 20 * '-')
+
+def check_password(psw):
+    if len(psw) < 8:
+        raise Exception("parola en az 7 karakter olmalıdır.")
+    elif not re.search("[a-z]", psw):
+        raise Exception("parola küçük harf içermelidir.")
+    elif not re.search("[A-Z]", psw):
+        raise Exception("parola büyük harf içermelidir.")
+    elif not re.search("[0-9]", psw):
+        raise Exception("parola rakam içermelidir.")
+    elif not re.search("[_@$]", psw):
+        raise Exception("parola alpha numeric karakter içermelidir.")
+    elif re.search(r"\s",psw):
+        raise Exception("parola boşluk içermemelidir.")
+    else:
+        print("geçerli parola: {}".format(psw))
+password = "1234567aA_"
+try:
+    check_password(password)
+except Exception as ex:
+    print(ex)
+finally:
+    print("validation tamamlandı.")
