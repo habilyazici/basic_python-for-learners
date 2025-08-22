55bd29791875729c06fad710e7ba4cafaf7c875d
Habil Yazıcı <habilyazici00@gmail.com>
2025-08-22 22:23:41 +0300

    lş

--- new_python/3-Modüller ve API/12_imdb.py
+++ new_python/3-Modüller ve API/12_imdb.py
@@ -2,9 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.imdb.com/chart/top?ref_=nv_mv_250"
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+html = requests.get(url, headers=headers).text
 
-html = requests.get(url).content
 soup = BeautifulSoup(html, "html.parser")
 
-yeni = soup.find_all("ul", attrs={"role": "presentation"})
-print("Yeni:", yeni)
+li_list = soup.find('ul', attrs={"role": "presentation"}).find_all('li')
+for li in li_list:
+    film_adi = li.find('h3', attrs={"class": "ipc-title__text"}).string
+    year = li.find('span', attrs={"class": "sc-15ac7568-7"}).string
+    rating = li.find("div", attrs={"data-testid": "ratingGroup--container"}).find("span").find("span").string
+    print(f"{film_adi}, Yıl: {year}, Rating: {rating}")
