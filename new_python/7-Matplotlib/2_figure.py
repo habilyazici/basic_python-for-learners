c14508b97750dfaccaaa8796856954922493b10d
Habil Yazıcı <habilyazici00@gmail.com>
2025-08-31 20:48:39 +0300

    s

--- new_python/7-Matplotlib/2_figure.py
+++ new_python/7-Matplotlib/2_figure.py
@@ -1,46 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 x = np.linspace(-10,9,20)
 y = x ** 3
 z = x ** 2
+figure = plt.figure(figsize=(8, 6))
+# figüre işimize lazım diye oluşturduk, otomatik oluşmaktadır
 
-figure = plt.figure()
-# kulanmazsakta otomatik olarak bir tane oluştur 
 axes_cube = figure.add_axes([0.1,0.1,0.8,0.8])
 # sol, alt, genişlik, yükseklik
-
-axes_cube.plot(x,y,'b')
+axes_cube.plot(x, y, color='blue', label='Cube')
+axes_cube.set_title("Cube")
 axes_cube.set_xlabel("X Axis")
 axes_cube.set_ylabel("Y Axis")
-axes_cube.set_title("Cube")
+axes_cube.legend(loc=4)
 
 axes_square = figure.add_axes([0.15,0.6,0.25,0.25])
-axes_square.plot(x,z,'r')
+axes_square.plot(x, z, color="yellow", label='Square')
+axes_square.set_title("Square")
 axes_square.set_xlabel("X Axis")
 axes_square.set_ylabel("Y Axis")
-axes_square.set_title("Square")
+axes_square.legend(loc=4)
 
-"""
-figure = plt.figure()
-
-axes = figure.add_axes([0,0,1,1])
-
-axes.plot(x,z,label="Square")
-axes.plot(x,y,label="Cube")
-axes.legend(loc=4)
-"""
-
-fig,axes = plt.subplots(nrows=2,ncols=1,figsize=(8,8))
-
-axes[0].plot(x,y)
-axes[0].set_title("Cube")
-axes[1].plot(x,z)
-axes[1].set_title("Square")
-
-plt.tight_layout()
-fig.savefig("figure2.pdf")
+figure.savefig("figure2.pdf")
 
 plt.show()
-
-
